cf6eaf1afe12ccdecff107d6390db5745af674c4
adrien lacomme <adrien.lacomme@hotmail.com>
2019-08-30 13:38:24 +0000

    Update GameManager.java

--- src/com/pacman/game/GameManager.java
+++ src/com/pacman/game/GameManager.java
@@ -1,39 +1,31 @@
 package com.pacman.game;
 
-//import java.awt.Color;
-//import java.awt.Font;
-//import java.awt.event.KeyEvent;
+import java.awt.Rectangle;
 
 import com.pacman.engine.Engine;
 import com.pacman.engine.IGame;
-//import com.pacman.engine.Inputs;
 import com.pacman.engine.Renderer;
+import com.pacman.game.DynamicObject;
 
 public class GameManager implements IGame
 {
-	//private int x = 0;
-	//private int y = 0;
+
+	Rectangle pacman = new Rectangle(10,10,50,50);
+	String direction = "right";
+
 	
 	@Override
 	public void init()
-	{	
+	{
+
 	}
 	
 	@Override
 	public void update(Engine engine)
 	{
-		/*Inputs input = engine.getInputs();
-		if ( input.isKeyDown( KeyEvent.VK_UP ) )
-		{
-			x--;
-			y--;
-		}
-		if ( input.isKeyDown( KeyEvent.VK_DOWN ) )
-		{
-			x++;
-			y++;
-		}*/
-		
+		direction = DynamicObject.getInstance().getNewDirection(direction);
+		DynamicObject.getInstance().updatePosition(pacman, direction);
+
 	}
 
 	@Override

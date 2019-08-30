b5c2cd032d6d7b066aa8d6d41fb676405066351c
adrien lacomme <adrien.lacomme@hotmail.com>
2019-08-30 13:38:55 +0000

    Add new file

---
+++ src/com/pacman/game/DynamicObject.java
@@ -0,0 +1,50 @@
+package com.pacman.game;
+
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+
+import com.pacman.engine.Engine;
+
+public class DynamicObject {
+	
+	
+	private static DynamicObject instance;
+	
+	public static DynamicObject getInstance()
+	{
+		if ( instance == null )
+		{
+			instance = new DynamicObject();		
+		}
+		
+		return instance;
+	}
+	
+	public String getNewDirection(String direction) {
+		if (Engine.getInstance().getInputs().isKeyDown(KeyEvent.VK_UP)) {
+			direction = "up";
+		} else if(Engine.getInstance().getInputs().isKeyDown(KeyEvent.VK_DOWN)) {
+			direction = "down";
+		} else if(Engine.getInstance().getInputs().isKeyDown(KeyEvent.VK_RIGHT)) {
+			direction = "right";
+		} else if(Engine.getInstance().getInputs().isKeyDown(KeyEvent.VK_LEFT)) {
+			direction = "left";
+		}
+		return direction;
+	}
+	
+	public void updatePosition(Rectangle object, String direction) {
+		if (direction.contentEquals("up")) {
+			object.setLocation((int)object.getX(),(int)object.getY()-1);
+		}
+		if (direction.contentEquals("down")) {
+			object.setLocation((int)object.getX(),(int)object.getY()+1);
+		}
+		if (direction.contentEquals("right")) {
+			object.setLocation((int)object.getX()+1,(int)object.getY());
+		}
+		if (direction.contentEquals("left")) {
+			object.setLocation((int)object.getX()-1,(int)object.getY());
+		}
+	}
+}

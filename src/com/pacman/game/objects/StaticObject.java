31498efc4356bb9b14e49833edc1e65c2ad0853b
Jordan <Jordan@10.238.168.25>
2019-09-01 13:56:45 -0400

    dev static object

--- src/com/pacman/game/objects/StaticObject.java
+++ src/com/pacman/game/objects/StaticObject.java
@@ -1,7 +1,21 @@
 package com.pacman.game.objects;
 
-public class StaticObject {
+public class StaticObject extends GameObject {
 
+	private static final long serialVersionUID = 1L;
+	protected boolean eaten;
+	protected int score;
+
+	public StaticObject() {
+		super();
+		this.eaten = false;
+		this.score = 0;
+	}
 	
-
+	public StaticObject(int x, int y, int width, int height, int score) {
+		super(x, y, width, height);
+		this.eaten = false;
+		this.score = score;
+	}
+	
 }

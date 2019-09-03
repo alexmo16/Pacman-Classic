94ac6bb3e9bb15e6dbc59c26aaa93801ee39a69f
Félix Roy <Felix.Roy@USherbrooke.ca>
2019-09-03 12:48:31 -0400

    More tests for the CSVUtils class

--- tests/com/pacman/unittests/CSVUtilsTest.java
+++ tests/com/pacman/unittests/CSVUtilsTest.java
@@ -12,6 +12,62 @@ import com.pacman.utils.CSVUtils;
 public class CSVUtilsTest 
 {
     @Test
+    public void test_object_construction() 
+    {        
+        CSVUtils result = new CSVUtils();
+        assertNotEquals(result, null);
+    }
+	
+    @Test
+    public void test_null_line() 
+    {
+        String line = null;
+        List<String> result = CSVUtils.parseLine(line);
+        
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 0);
+    }
+	
+    @Test
+    public void test_empty_line() 
+    {
+        String line = "";
+        List<String> result = CSVUtils.parseLine(line);
+        
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 0);
+    }
+    
+    @Test
+    public void test_end_of_line_lf() 
+    {
+        String line = "10,AU,Australia\n11,CA,Canada";
+        List<String> result = CSVUtils.parseLine(line);
+        
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(0), "10");
+        assertEquals(result.get(1), "AU");
+        assertEquals(result.get(2), "Australia");
+    }
+    
+    @Test
+    public void test_end_of_line_cr() 
+    {
+        String line = "10,AU,Australia,\r11,CA,Canada";
+        List<String> result = CSVUtils.parseLine(line);
+        
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 6);
+        assertEquals(result.get(0), "10");
+        assertEquals(result.get(1), "AU");
+        assertEquals(result.get(2), "Australia");
+        assertEquals(result.get(3), "11");
+        assertEquals(result.get(4), "CA");
+        assertEquals(result.get(5), "Canada");
+    }
+    
+    @Test
     public void test_no_quote() 
     {
         String line = "10,AU,Australia";
@@ -61,6 +117,32 @@ public class CSVUtilsTest
         assertEquals(result.get(0), "10");
         assertEquals(result.get(1), "AU");
         assertEquals(result.get(2), "Aus,tralia");
+    }
+    
+    @Test
+    public void test_custom_empty_space_separator()
+    {
+        String line = "10,AU,Australia";
+        List<String> result = CSVUtils.parseLine(line, ' ');
+
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(0), "10");
+        assertEquals(result.get(1), "AU");
+        assertEquals(result.get(2), "Australia");
+    }
+    
+    @Test
+    public void test_custom_empty_separator_and_quote()
+    {
+        String line = "10,AU,Australia";
+        List<String> result = CSVUtils.parseLine(line, ' ', ' ');
+
+        assertNotEquals(result, null);
+        assertEquals(result.size(), 3);
+        assertEquals(result.get(0), "10");
+        assertEquals(result.get(1), "AU");
+        assertEquals(result.get(2), "Australia");
     }
     
     @Test

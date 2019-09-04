80a5b2c5541faeb4d0411e4919eddd1157420039
Adrien <Adrien@10.238.186.21>
2019-09-04 15:57:09 -0400

    utest

--- tests/com/pacman/unittests/game/objects/PacmanObjectTests.java
+++ tests/com/pacman/unittests/game/objects/PacmanObjectTests.java
@@ -1,15 +1,22 @@
 package com.pacman.unittests.game.objects;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.event.KeyEvent;
+
+
+import org.mockito.Mockito;
 
 import org.junit.jupiter.api.Test;
 
+import com.pacman.engine.Inputs;
 import com.pacman.game.Settings;
 import com.pacman.game.objects.PacmanObject;
 
 public class PacmanObjectTests {
 	
-	static Settings settings = new Settings();
+	Settings settings = new Settings();
+	Inputs inputs = Mockito.mock( Inputs.class );
 	PacmanObject pacmanOk = new PacmanObject(10,10,10,10,"right",settings);
 	PacmanObject pacmanRightWrongPosi = new PacmanObject(10,10,10,10,"",settings);
 	String direction;
@@ -19,11 +26,11 @@ public class PacmanObjectTests {
 	void test_void_pacman_object_creation() {
 		boolean isException = false;
 		try {
-		      new PacmanObject();
+		      new PacmanObject(settings);
 		    } catch (Exception e) {
 		      isException = true;
 		    }
-		assertTrue( isException );
+		assertFalse( isException );
 
 	}
 	
@@ -35,7 +42,7 @@ public class PacmanObjectTests {
 		    } catch (Exception e) {
 		      isException = true;
 		    }
-		assertTrue( isException );
+		assertFalse( isException );
 
 	}
 	
@@ -47,7 +54,7 @@ public class PacmanObjectTests {
 		    } catch (Exception e) {
 		      isException = true;
 		    }
-		assertTrue( isException );
+		assertFalse( isException );
 
 	}
 	
@@ -59,13 +66,16 @@ public class PacmanObjectTests {
 		    } catch (Exception e) {
 		      isException = true;
 		    }
-		assertTrue( isException );
+		assertFalse( isException );
 
 	}
 	
 	@Test
 	void test_new_direction_pacman_object() {
+		pacmanOk.checkNewDirection(inputs);
+		Mockito.when( inputs.isKeyDown(KeyEvent.VK_UP) ).thenReturn( true );
 
+		assertEquals("up",pacmanOk.getDirection());
 
 	}
 }

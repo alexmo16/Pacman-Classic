bdbcbc3c93e83bf0480f63765e60bcc8a5e6c1c1
Félix Roy <Felix.Roy@USherbrooke.ca>
2019-09-04 15:29:49 -0400

    Adding an enum for direction

--- src/com/pacman/game/objects/DynamicObject.java
+++ src/com/pacman/game/objects/DynamicObject.java
@@ -76,4 +76,21 @@ public abstract class DynamicObject extends GameObject
             break;
         }
     }
+    
+    public enum Direction
+    {
+    	RIGHT(0),
+        LEFT(1),
+        UP(2),
+        DOWN(3);
+
+        private final int value;
+
+        DynamicObjectDirection(final int newValue)
+        {
+            value = newValue;
+        }
+
+        public int getValue() { return value; }
+    }
 }

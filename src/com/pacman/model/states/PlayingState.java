cae5b143298acbf08985747e33611c7ece31ca6e
Jordan <Jordan@192.168.2.107>
2019-09-15 14:59:52 -0400

    Format the code

--- src/com/pacman/model/states/PlayingState.java
+++ src/com/pacman/model/states/PlayingState.java
@@ -20,177 +20,175 @@ import com.pacman.utils.Settings;
 
 public class PlayingState implements IGameState, IObserver<Direction>
 {
-	private StatesName name = StatesName.PLAY;
-	
-	private Game game;
-	private Collision collision;
-	private int i;
-	private volatile boolean isPacmanDying = false;
-	private Random random;
-	private int randomInt;
-	
+    private StatesName name = StatesName.PLAY;
+
+    private Game game;
+    private Collision collision;
+    private int i;
+    private volatile boolean isPacmanDying = false;
+    private Random random;
+    private int randomInt;
+
     private TimerThread timerThread;
 
-	LineListener deathSoundListener = new LineListener()
-	{
-	     @Override
-	     public void update(LineEvent event)
-	     {
-	         if (event.getType() == LineEvent.Type.STOP)
-	         {		
-	        	game.setState(game.getPacman().getLives() == 0 ? game.getStopState() : game.getInitState());
-	   			game.stopDeathMusic();
-	   			isPacmanDying = false;
-	         }
-	     }
-	};
-	
-	public PlayingState( Game gm )
-	{
-		if ( gm == null )
-		{
-			return;
-		}
-		
-		game = gm;
-		
-		collision = game.getCollision();
-		
-		game.getPacman().registerObserver(this);
-        
-		game.getNewDirectionPacman().getHitBox().setRect(game.getPacman().getHitBox());
-		game.getNextTilesPacman().getHitBox().setRect(game.getPacman().getHitBox());
-        
+    LineListener deathSoundListener = new LineListener()
+    {
+        @Override
+        public void update(LineEvent event)
+        {
+            if (event.getType() == LineEvent.Type.STOP)
+            {
+                game.setState(game.getPacman().getLives() == 0 ? game.getStopState() : game.getInitState());
+                game.stopDeathMusic();
+                isPacmanDying = false;
+            }
+        }
+    };
+
+    public PlayingState(Game gm)
+    {
+        if (gm == null)
+        {
+            return;
+        }
+
+        game = gm;
+
+        collision = game.getCollision();
+
+        game.getPacman().registerObserver(this);
+
+        game.getNewDirectionPacman().getHitBox().setRect(game.getPacman().getHitBox());
+        game.getNextTilesPacman().getHitBox().setRect(game.getPacman().getHitBox());
+
         game.setNextTilesDirection(game.getPacman().getDirection());
         game.setNewDirection(game.getNextTilesDirection());
-	}
+    }
 
-	@Override
-	public void update() 
-	{
-		if (isPacmanDying)
-		{
-			if (game.getPacman().getCurrentAnimation() == Animation.DYING && game.getPacman().isEndOfAnimation())
-			{
-				game.setState(game.getPacman().getLives() <= 0 ? game.getStopState() : game.getInitState());
-				isPacmanDying = false;
-			}
-			return;
-		}
-		
+    @Override
+    public void update()
+    {
+        if (isPacmanDying)
+        {
+            if (game.getPacman().getCurrentAnimation() == Animation.DYING && game.getPacman().isEndOfAnimation())
+            {
+                game.setState(game.getPacman().getLives() <= 0 ? game.getStopState() : game.getInitState());
+                isPacmanDying = false;
+            }
+            return;
+        }
+
         if (timerThread == null)
-		{
-        	
-        	timerThread = new TimerThread(5);
-			timerThread.start();
-		}
-		
-		if (!timerThread.isAlive())
-		{
-			random = new Random();
-			randomInt = random.nextInt(4);
+        {
 
-			if ( !((Ghost) game.getEntities().get(randomInt)).getAlive())
-			{
-				
-				collision.ghostSpawn(((Ghost)game.getEntities().get(randomInt)));
-				((Ghost)game.getEntities().get(randomInt)).setSpawning();
-				timerThread = null;
-			}
-			
-		}
-		
-		Level level = game.getCurrentLevel();
-		ArrayList<PacDot> pacdots = level.getPacDots();
-		if (pacdots.size() == 0)
-		{
-			game.setPacmanWon(true);
-			game.setState(game.getStopState());
-		}
-		
-		
-		for (Entity entity : game.getEntities()) {
-			if (entity.getHitBox() != game.getPacman().getHitBox())
-			{
-				if (((Ghost)entity).getAlive())
-				{
+            timerThread = new TimerThread(5);
+            timerThread.start();
+        }
 
-					Ghost g2 = new Ghost( ((Ghost)entity).getX(),((Ghost)entity).getY(), ((Ghost)entity).getType() );
-					g2.setDirection(((Ghost)entity).getDirection());
-					g2.updatePosition(g2.getDirection());
-					while (collision.collisionWall(g2) != "path")
-					{
-						g2 = new Ghost( ((Ghost)entity).getX(),((Ghost)entity).getY(), ((Ghost)entity).getType() );
-						g2.setDirection(((Ghost)entity).getDirection());
-						g2.getNewDirection();
-						g2.updatePosition(g2.getDirection());
-						
-						
-					}
-					entity.setDirection(g2.getDirection());
-					entity.updatePosition(g2.getDirection());
-					
-				} else if (((Ghost)entity).getSpawning())
-				{
-					collision.ghostSpawn(entity);
-				}
-			}
-		}
-		
-		for (i = 0; i < Settings.SPEED; i++ )
-		{
-			
-			game.getNewDirectionPacman().getHitBox().setRect(game.getPacman().getHitBox());
-			game.getNextTilesPacman().getHitBox().setRect(game.getPacman().getHitBox());
-	        game.getNextTilesPacman().updatePosition(game.getNextTilesDirection());
-	        game.getNewDirectionPacman().updatePosition(game.getNewDirection());
+        if (!timerThread.isAlive())
+        {
+            random = new Random();
+            randomInt = random.nextInt(4);
 
-	        
-	        if (collision.collisionGhost())
-	        {
-	        	game.killPacman();
-	        	return;
-	        }
+            if (!((Ghost) game.getEntities().get(randomInt)).getAlive())
+            {
 
-	        // Strategy pattern for wall collisions.
+                collision.ghostSpawn(((Ghost) game.getEntities().get(randomInt)));
+                ((Ghost) game.getEntities().get(randomInt)).setSpawning();
+                timerThread = null;
+            }
 
-	        collision.checkConsumablesCollision();
-	        collision.executeWallStrategy();
-		}
-		
-	}
+        }
 
-	@Override
-	public StatesName getName()
-	{
-		return name;
-	}
-    
+        Level level = game.getCurrentLevel();
+        ArrayList<PacDot> pacdots = level.getPacDots();
+        if (pacdots.size() == 0)
+        {
+            game.setPacmanWon(true);
+            game.setState(game.getStopState());
+        }
+
+        for (Entity entity : game.getEntities())
+        {
+            if (entity.getHitBox() != game.getPacman().getHitBox())
+            {
+                if (((Ghost) entity).getAlive())
+                {
+
+                    Ghost g2 = new Ghost(((Ghost) entity).getX(), ((Ghost) entity).getY(), ((Ghost) entity).getType());
+                    g2.setDirection(((Ghost) entity).getDirection());
+                    g2.updatePosition(g2.getDirection());
+                    while (collision.collisionWall(g2) != "path")
+                    {
+                        g2 = new Ghost(((Ghost) entity).getX(), ((Ghost) entity).getY(), ((Ghost) entity).getType());
+                        g2.setDirection(((Ghost) entity).getDirection());
+                        g2.getNewDirection();
+                        g2.updatePosition(g2.getDirection());
+
+                    }
+                    entity.setDirection(g2.getDirection());
+                    entity.updatePosition(g2.getDirection());
+
+                } else if (((Ghost) entity).getSpawning())
+                {
+                    collision.ghostSpawn(entity);
+                }
+            }
+        }
+
+        for (i = 0; i < Settings.SPEED; i++)
+        {
+
+            game.getNewDirectionPacman().getHitBox().setRect(game.getPacman().getHitBox());
+            game.getNextTilesPacman().getHitBox().setRect(game.getPacman().getHitBox());
+            game.getNextTilesPacman().updatePosition(game.getNextTilesDirection());
+            game.getNewDirectionPacman().updatePosition(game.getNewDirection());
+
+            if (collision.collisionGhost())
+            {
+                game.killPacman();
+                return;
+            }
+
+            // Strategy pattern for wall collisions.
+
+            collision.checkConsumablesCollision();
+            collision.executeWallStrategy();
+        }
+
+    }
+
+    @Override
+    public StatesName getName()
+    {
+        return name;
+    }
+
     /**
      * update of the observer
      */
-	@Override
-	public void update(Direction d) 
-	{
+    @Override
+    public void update(Direction d)
+    {
 
-			if ( game.getNewDirection() == game.getPacman().getDirection() ) 
-			{
-			game.setNextTilesDirection(game.getNewDirection());
-			}
-				game.setNewDirection(d);
-			
-	}
-	
-	public void killPacman()
-	{
-		isPacmanDying = true;
-		game.stopInGameMusics();
-		game.getPacman().looseLive();
-		game.playDeathMusic();
-		
-		for (int i = 0; i<4; i++)
-		{
-			((Ghost) game.getEntities().get(i)).respawn();
-		}
-	}
+        if (game.getNewDirection() == game.getPacman().getDirection())
+        {
+            game.setNextTilesDirection(game.getNewDirection());
+        }
+        game.setNewDirection(d);
+
+    }
+
+    public void killPacman()
+    {
+        isPacmanDying = true;
+        game.stopInGameMusics();
+        game.getPacman().looseLive();
+        game.playDeathMusic();
+
+        for (int i = 0; i < 4; i++)
+        {
+            ((Ghost) game.getEntities().get(i)).respawn();
+        }
+    }
 }
